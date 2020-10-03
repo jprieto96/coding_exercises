b4b82d549c62f3f7211f52ed3e3f0f577949350c
jprieto96 <joseprieto9657@gmail.com>
2020-10-03 21:33:40 +0200

    initialize the repository

---
+++ main.py
@@ -0,0 +1,13 @@
+from binarytree import Node
+
+# Create some examples of binary trees
+create_binary_trees()
+
+def create_binary_trees():
+    pass
+
+if __name__ == "__main__":
+    pass
+    
+
+    
